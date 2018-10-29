5aa607e6d519d4aae0638bde5617168002664c6b
David Sullivan <dsullivan0190@gmail.com>
2018-10-29 11:19:18 +0000

    Added LOGGING_NAMESPACE variable

--- dp4py_sanic/config/config.py
+++ dp4py_sanic/config/config.py
@@ -1,7 +1,9 @@
 """
 Contains server config
 """
+import os
 import logging
+
 from dp4py_config.section import Section
 from dp4py_config.utils import bool_env
 
@@ -13,6 +15,7 @@ CONFIG.APP.title = 'dp4py-sanic'
 CONFIG.APP.description = 'Sanic app wrapper for digital publishing that implements JSON logging'
 
 CONFIG.LOGGING = Section("Logging config")
+CONFIG.LOGGING.namespace = os.environ.get("LOGGING_NAMESPACE", CONFIG.APP.title)
 CONFIG.LOGGING.default_level = logging.INFO
 CONFIG.LOGGING.coloured_logging = bool_env('COLOURED_LOGGING_ENABLED', False)
 CONFIG.LOGGING.pretty_logging = bool_env('PRETTY_LOGGING', False)
--- dp4py_sanic/logging/formatter.py
+++ dp4py_sanic/logging/formatter.py
@@ -8,8 +8,11 @@ from dp4py_logging.log.formatters import CustomJsonFormatter
 class SanicJsonFormatter(CustomJsonFormatter):
     def __init__(self, *args, **kwargs):
         super(SanicJsonFormatter, self).__init__(
-            *args, json_indent=CONFIG.LOGGING.json_logger_indent, coloured_logging=CONFIG.LOGGING.coloured_logging, **kwargs)
+            *args,
+            json_indent=CONFIG.LOGGING.json_logger_indent,
+            coloured_logging=CONFIG.LOGGING.coloured_logging,
+            **kwargs)
 
     def add_fields(self, log_record, record, message_dict):
         super(SanicJsonFormatter, self).add_fields(log_record, record, message_dict)
-        log_record['namespace'] = CONFIG.APP.title
+        log_record['namespace'] = CONFIG.LOGGING.namespace
