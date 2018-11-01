9032db3897d474e1d4d186804f4166fa23dc412f
David Sullivan <dsullivan0190@gmail.com>
2018-11-01 13:48:54 +0000

    Default to own Request class

--- dp4py_sanic/app/server.py
+++ dp4py_sanic/app/server.py
@@ -6,13 +6,15 @@ import logging
 from sanic import Sanic
 from sanic_prometheus import monitor
 
+from dp4py_sanic.app.request import Request
+
 from dp4py_sanic.config import CONFIG
 from dp4py_sanic.logging.log_config import log_config
 
 
 class Server(Sanic):
     def __init__(self, name=None, router=None, error_handler=None,
-                 load_env=True, request_class=None,
+                 load_env=True, request_class=Request,
                  strict_slashes=False, log_config=log_config,
                  configure_logging=True):
         super(Server, self).__init__(name=name, router=router, error_handler=error_handler,
